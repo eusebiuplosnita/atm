beacdb553698d9cbb42f931d1d309c91c1eff694
Eusebiu Plosnita <eusebiu.plosnita@gmail.com>
2020-03-20 09:58:46 +0200

    Implemented executeTransaction and made a few changes in some classes.

--- src/main/java/com/interview/ing/atm/machine/model/BankAccount.java
+++ src/main/java/com/interview/ing/atm/machine/model/BankAccount.java
@@ -1,20 +1,25 @@
 package com.interview.ing.atm.machine.model;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Transient;
+
+import com.interview.ing.atm.machine.service.Transaction;
 
 @Entity
 @Table(name="bankAccounts")
 public class BankAccount {
 	
 	@Id
-	@Column(name = "id")
+	@Column(name = "bankAccountId")
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int id;
+	private String bankAccountId;
 	
 	@Column(name = "ibanCode")
 	private String ibanCode;
@@ -22,8 +27,23 @@ public class BankAccount {
 	@Column(name = "balance")
 	private double balance;
 	
+	@Transient
+	private List<Transaction> transactions;
+	
 	public BankAccount() {}
+	
+	public BankAccount(String id) {
+		this.setBankAccountId(id);
+	}
 
+	public String getBankAccountId() {
+		return this.bankAccountId;
+	}
+	
+	public void setBankAccountId(String id) {
+		this.bankAccountId = id;
+	}
+	
 	public String getIbanCode() {
 		return ibanCode;
 	}
@@ -38,5 +58,13 @@ public class BankAccount {
 
 	public void setBalance(double balance) {
 		this.balance = balance;
+	}
+	
+	public List<Transaction> getTransactions() {
+		return this.transactions;
+	}
+	
+	public void setTransactions(List<Transaction> transactions) {
+		this.transactions = transactions;
 	}
 }
--- src/main/java/com/interview/ing/atm/machine/service/Transaction.java
+++ src/main/java/com/interview/ing/atm/machine/service/Transaction.java
@@ -1,5 +1,36 @@
 package com.interview.ing.atm.machine.service;
 
+import java.time.Instant;
+
 public class Transaction {
 
+	private String type;
+	
+	private double amount;
+	
+	private Instant date;
+
+	public Instant getDate() {
+		return date;
+	}
+
+	public void setDate(Instant date) {
+		this.date = date;
+	}
+
+	public double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
 }
